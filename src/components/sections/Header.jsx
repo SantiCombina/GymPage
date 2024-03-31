2efbb6b2a798bf399e4c3cacc06ab65092f298f8
SantiCombina <santicombina@gmail.com>
2024-03-30 23:51:44 -0300

    Fixed header responsive

--- src/components/sections/Header.jsx
+++ src/components/sections/Header.jsx
@@ -6,9 +6,9 @@ export const Header = () => {
   return (
     <section
       ref={headerRef}
-      className="w-full h-screen bg-center bg-no-repeat bg-cover bg-hero-main"
+      className="w-full h-screen px-6 bg-center bg-no-repeat bg-cover bg-hero-main"
     >
-      <h1 className="flex items-center justify-center h-full text-6xl font-extrabold">
+      <h1 className="flex items-center justify-center w-full h-full text-4xl font-extrabold md:text-6xl">
         EMPIEZA HOY
       </h1>
     </section>
