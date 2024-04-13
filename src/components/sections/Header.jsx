f03d6f9ecd96212ddbb8f3993569b92057223293
SantiCombina <santicombina@gmail.com>
2024-04-13 15:10:54 -0300

    Refactored header

--- src/components/sections/Header.jsx
+++ src/components/sections/Header.jsx
@@ -6,9 +6,9 @@ export const Header = () => {
   return (
     <section
       ref={headerRef}
-      className="w-full h-screen px-6 bg-center bg-no-repeat bg-cover bg-hero-main"
+      className="flex items-center justify-center w-full min-h-screen px-6 bg-center bg-no-repeat bg-cover bg-hero-main"
     >
-      <h1 className="flex items-center justify-center w-full h-full text-4xl font-extrabold md:text-6xl">
+      <h1 className="text-4xl font-extrabold md:text-6xl">
         EMPIEZA HOY
       </h1>
     </section>
